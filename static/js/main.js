e07b7de7c34731295d8ca23a97408f83ce803d8c
Christiaan Van den Poel <christiaan.vandenpoel@gmail.com>
2025-12-06 12:15:58 +0100

    first version

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+// Slideshow functionality
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initialize first slide
+showSlide(0);
+
+// Change slide every 5 seconds
+setInterval(nextSlide, 5000);
