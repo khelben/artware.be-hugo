a1a23255e3c7883f6fbad9da5645cfe6d7cc5b5d
Christiaan Van den Poel <christiaan.vandenpoel@gmail.com>
2026-03-01 16:23:17 +0100

    added more slides

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,10 @@
 // Slideshow functionality
+const slideshowContainer = document.querySelector('.slideshow');
+console.dir(slideshowContainer);
+for (let index = 1; index <= 55; index++) {
+    slideshowContainer.innerHTML += `<div class="slide" style="background-image: url('/images/slide${index}.jpg');"></div>`;
+}
+
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
