a4141e370f41c36411c7524ba63410fd8a55ba24
Christiaan Van den Poel <christiaan.vandenpoel@gmail.com>
2026-03-15 19:46:36 +0100

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,7 @@
 import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe@5/dist/photoswipe-lightbox.esm.js';
 
-const lightbox = new PhotoSwipeLightbox({
-    gallery: '#gallery-grid',
-    children: 'a',
-    pswpModule: () => import('https://unpkg.com/photoswipe@5/dist/photoswipe.esm.js'),
-});
+const pswpModule = () => import('https://unpkg.com/photoswipe@5/dist/photoswipe.esm.js');
 
-const processLightbox = new PhotoSwipeLightbox({
-    gallery: '#process-grid',
-    children: 'a',
-    pswpModule: () => import('https://unpkg.com/photoswipe@5/dist/photoswipe.esm.js'),
-});
-
-const lightboxGray = new PhotoSwipeLightbox({
-    gallery: '#gallery-grid-gray',
-    children: 'a',
-    pswpModule: () => import('https://unpkg.com/photoswipe@5/dist/photoswipe.esm.js'),
-});
-
-lightbox.init();
-processLightbox.init();
-lightboxGray.init();
+['#gallery-grid', '#gallery-grid-gray', '#process-grid']
+    .filter(id => document.querySelector(id))
+    .forEach(id => new PhotoSwipeLightbox({ gallery: id, children: 'a', pswpModule }).init());
